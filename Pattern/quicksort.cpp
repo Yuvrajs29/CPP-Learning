d53af262d57ad9745394b80d063ffa3667b1eb06
Yuvraj Singh <yuvrajsingh655709@gmail.com>
2025-09-09 19:27:58 +0530

    C++ STL

---
+++ Pattern/quicksort.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; 
+    int i = (low - 1);
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) { 
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);   
+        quickSort(arr, pi + 1, high); 
+    }
+}
+
+int main() {
+    int arr[] = {15, 56, 62, 2, 9, 16, 21, 17, 23, 3, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unsorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
